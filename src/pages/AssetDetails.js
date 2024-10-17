54586cceabebde19ace163ff5728d26bdb7b5f95
Ben Joseph <benjosephcr7@gmail.com>
2024-10-17 19:43:00 +0530

    changes

--- src/pages/AssetDetails.js
+++ src/pages/AssetDetails.js
@@ -1,31 +1,51 @@
 import React, { useEffect, useState } from "react";
 import AssetForm from "../components/AssetForm";
-import axios from "axios";
+//import axios from "axios";
 import { Bars } from "react-loader-spinner";
 import { DataGrid } from "@mui/x-data-grid";
 import { Box } from "@mui/material";
 
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'name', headerName: 'Asset Name', width: 150 },
-    { field: 'value', headerName: 'Asset Value ($)', width: 130 },
-];
-
-const rows = [
-    { id: 1, name: 'Laptop', value: 1500 },
-    { id: 2, name: 'Phone', value: 800 },
-    { id: 3, name: 'Monitor', value: 300 },
-];
-
 const AssetDetails = () => {
 
+    const columns = [
+        { field: 'id', headerName: 'AssetId', width: 100 },
+        { field: 'investmentEntity', headerName: 'InvestmentEntity', width: 100 },
+        { field: 'investmentType', headerName: 'Type', width: 100 },
+        { field: 'amount', headerName: 'Amount', width: 100 },
+        { field: 'interestRate', headerName: 'InterestRate', width: 100 },
+        { field: 'interestFrequency', headerName: 'InterestFrequency', width: 100 },
+        { field: 'userId', headerName: 'UserId', width: 100 },
+        { field: 'startDate', headerName: 'StartDate', width: 100 },
+        { field: 'maturityDate', headerName: 'MaturityDate', width: 100 },
+        { field: 'asOfDate', headerName: 'AsOfDate', width: 100 },
+        { field: 'remarks', headerName: 'Remarks', width: 100 },
+    ];
+
+
+    const [data, setData] = useState([]);
+
+    const [page,setPageSize]=useState([25]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://networthtrackerapi20240213185304.azurewebsites.net/api/General/getAssetDetails');
+                const result = await response.json();
+                setData(result);
+            }
+            catch (error) {
+                console.log("Error fetching data: ", error);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <div>
             <Box sx={{ height: 400, width: '100%' }}>
                 <h1>Asset Report</h1>
-                <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[5]} />
+                <DataGrid rows={data} columns={columns} pageSize={page} onPageSizeChange={(newPageSize) => setPageSize(newPageSize)} rowsPerPageOptions={[5,10,25,50]} />
             </Box>
         </div>
     );
